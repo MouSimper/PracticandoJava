9ee374a61f36b97adef61f38ff2b6d796db5acbf
MouSimper <diegopoe09@gmail.com>
2026-04-09 15:13:03 -0500

    Esto es del Intelli

---
+++ PRACTICAS/src/Presentate.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+public class Presentate {
+    public static void main(String[] args) {
+        System.out.println("Diego Huaman");
+        System.out.println("22");
+        System.out.println("Perú");
+    }
+}
