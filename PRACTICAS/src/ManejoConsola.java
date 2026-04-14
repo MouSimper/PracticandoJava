a04316d37eb766ec534bd52735c8c843060fa276
MouSimper <diegopoe09@gmail.com>
2026-04-14 15:35:38 -0500

    MANEJO DEL SCANNER

---
+++ PRACTICAS/src/ManejoConsola.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ManejoConsola {
+    public static void main(String[] args) {
+        var consola = new Scanner(System.in);
+        System.out.println("Escribe tu nombre: ");
+        var nombre = consola.nextLine();
+        System.out.println("nombre = " + nombre);
+        System.out.println("Ingresa tu edad: ");
+        var edad = consola.nextInt();
+        System.out.println("Edad: "+edad);
+    }
+}
